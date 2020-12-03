a09b8db141515d9b859199cda7783bd66cfb2692
almahmoa <55867275+almahmoa@users.noreply.github.com>
2020-12-03 14:34:44 -0800

    Update hw6.js

--- hw6.js
+++ hw6.js
@@ -76,10 +76,10 @@ app.get('/safe-update',function(req,res,next){
 		for(var i in rows) {
 				workoutArray.push({'id':rows[i].id, 'name': rows[i].name, 'reps':rows[i].reps, 'weight':rows[i].weight, 'date':rows[i].date, 'unit':rows[i].unit}) 
 		}
-		context.workouts = workoutArray;
-		res.render('home',context); 
       });
     }
+	context.workouts = workoutArray;
+	res.render('home',context); 
   });
 });
 
